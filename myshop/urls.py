228ed61dcfab7211d71aace71dc9a6bd6dedf422
swksprojects <swkshannon@gmail.com>
2017-09-17 10:27:32 +0100

    kex_team orphan initial commit

---
+++ myshop/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+import cart.urls
+import shop.urls
+import orders.urls
+import payment.urls
+import paypal.standard.ipn.urls
+
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^cart/', include(cart.urls, namespace='cart')),
+    url(r'^orders/', include(orders.urls, namespace='orders')),
+    url(r'paypal/', include(paypal.standard.ipn.urls)),
+    url(r'payment', include(payment.urls, namespace='payment')),
+    url(r'^', include(shop.urls, namespace='shop')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
