f81e137b4d813453030a27af0cafc28620a8896d
swksprojects <swkshannon@gmail.com>
2017-10-08 10:06:04 +0100

    prepare to add postgres

--- kextech_site/settings.py
+++ kextech_site/settings.py
@@ -95,6 +95,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'mydatabaseuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
