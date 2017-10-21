508ea945f2572789fd95198f0fc005fe355308cd
swksprojects <swkshannon@gmail.com>
2017-10-21 21:07:15 +0100

    tweak celery start up - hopefully fixes emails

--- kextech_site/settings.py
+++ kextech_site/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',
+    # 'storages',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
