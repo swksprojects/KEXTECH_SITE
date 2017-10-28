d2829519f35cc39493fe2e2c8ffa7511751aa00f
swksprojects <swkshannon@gmail.com>
2017-10-28 22:18:08 +0100

    edited settings file for local development

--- kextech_site/settings.py
+++ kextech_site/settings.py
@@ -96,22 +96,22 @@ WSGI_APPLICATION = 'kextech_site.wsgi.application'
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'kextechdb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'K/Xdb=dbt3K',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kextechdb',
+        'USER': 'postgres',
+        'PASSWORD': 'K/Xdb=dbt3K',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 
-import dj_database_url
-
-DATABASES = {"default": dj_database_url.config()}
+# import dj_database_url
+#
+# DATABASES = {"default": dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -150,34 +150,34 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-# STATIC_URL = '/static/'
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'shop')
-#
-#
-# MEDIA_URL = '/media/'
-#
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'shop')
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
 #set S3 as the place to store your files.
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-AWS_QUERYSTRING_AUTH = False  #This will make sure that the file URL does not have unnecessary parameters like your access key.
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com"
-#static media settings
-STATIC_URL = "https://" + AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com/"
-MEDIA_URL = STATIC_URL + "media/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-STATIC_ROOT = "staticfiles"
-ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
-STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-)
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+# AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+# AWS_QUERYSTRING_AUTH = False  #This will make sure that the file URL does not have unnecessary parameters like your access key.
+# AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com"
+# #static media settings
+# STATIC_URL = "https://" + AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com/"
+# MEDIA_URL = STATIC_URL + "media/"
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+# STATIC_ROOT = "staticfiles"
+# ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
+# STATICFILES_FINDERS = (
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+# )
 
 
 # Cart Session ID
