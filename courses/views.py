f9d14f0a8890f1947936f3176cf8387864567f41
swksprojects <swkshannon@gmail.com>
2017-11-01 22:17:45 +0000

    creating course views - work in progress

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
 
-# Create your views here.
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_query_set()
+        return qs.filter(owner=self.request.user)
+
